81c63747bd810e74eceb2f433b00539244a611ec
nabeel <nabeelali720@gmail.com>
2023-11-02 18:48:54 +0500

    first

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import TermsCondition from './Components/TermsCondition'
+// import DealsOffer from './Components/DealsOffer'
+// import PrivacyPolicy from './Components/PrivacyPolicy'
+// import HotelCardDetails from './Components/HotelCardDetails'
+// import Navigation from './Config/Navigation'
+// import FoodPreview from './Components/FoodPreview'
+// import Footer from './Components/Footer'
+
+function App() {
+  return (
+    <div>
+      {/* <Navigation/> */}
+      {/* <FoodPreview/> */}
+
+
+<TermsCondition/>
+
+{/* <PrivacyPolicy/> */}
+      {/* <HotelCardDetails/> */}
+
+      {/* <DealsOffer/> */}
+
+    </div>
+  )
+}
+
+export default App
